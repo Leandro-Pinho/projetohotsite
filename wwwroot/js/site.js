d94565efd6376088e3945bdb7a964deaaff847e9
Leandro-Pinho <77171351+Leandro-Pinho@users.noreply.github.com>
2021-07-02 19:22:35 -0300

    Incluir a ação pop-up

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+$('#formContato').on('submit', function(e){
+    $('#modalConfirma').modal('show');
+    e.preventDefault();
+});
+
+function Evento(e){
+    $('#modalEvento').modal('show');
+    e.preventDefault();
+};
+
+function Dica(e){
+    $('#modalDicas').modal('show');
+    e.preventDefault();
+};
+
+function Apoio(e){
+    $('#modalApoio').modal('show');
+    e.preventDefault();
+};
